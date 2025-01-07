de95a7915247c5e89a6490f6468d7ce4368a7a0d
Caroline Yamashita Caleguer <cayamash@student.42sp.org.br>
2025-01-07 18:51:06 -0300

    started main funcions in server and client

--- src/client/client.c
+++ src/client/client.c
@@ -3,17 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cayamash <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: cayamash <cayamash@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 17:26:10 by cayamash          #+#    #+#             */
-/*   Updated: 2025/01/07 17:26:53 by cayamash         ###   ########.fr       */
+/*   Updated: 2025/01/07 18:25:24 by cayamash         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
-	ft_printf("Cliente");
+	if(argc != 3)
+	{
+		ft_printf("Executable Usage: ./client [PID] [message]\n");
+		return (1);
+	}
+	while (argc--)
+		ft_printf("%s\n", argv[argc]);
 	return (0);
 }
--- src/server/server.c
+++ src/server/server.c
@@ -3,19 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cayamash <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: cayamash <cayamash@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 15:55:36 by cayamash          #+#    #+#             */
-/*   Updated: 2025/01/07 15:57:38 by cayamash         ###   ########.fr       */
+/*   Updated: 2025/01/07 18:36:28 by cayamash         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+//if SIGUSR1: bit = 1
+//if SIGUSR2: bit = 2
+
+void	handler(int signo, siginfo_t *info, void *context);
+
 int	main(void)
 {
 	pid_t pid = getpid();
 
-	ft_printf("PID: %d", pid);
+	ft_printf("PID Server: %d\n", pid);
+
+	while (1)
+		pause();
+	
 	return (0);
 }
