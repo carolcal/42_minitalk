f855c54a220e4d9b925649d93d367111760070c0
Caroline Yamashita Caleguer <cayamash@student.42sp.org.br>
2025-01-08 16:52:05 -0300

    print char + response signal to client

--- src/client.c
+++ src/client.c
@@ -6,31 +6,41 @@
 /*   By: cayamash <cayamash@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 17:26:10 by cayamash          #+#    #+#             */
-/*   Updated: 2025/01/08 11:28:51 by cayamash         ###   ########.fr       */
+/*   Updated: 2025/01/08 16:50:35 by cayamash         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+static int	g_response = 0;
+
+void response_handler(int signal)
+{
+	if (signal == SIGUSR1)
+		g_response = 1;
+}
+
 void	send_message(int pid, char *message)
 {
 	int	i;
 	int	j;
-	int	bit;
+	int	len;
 
 	i = 0;
-	while (message[i] != '\0')
+	len = ft_strlen(message);
+	while (i <= len)
 	{
 		j = 7;
 		while (j >= 0)
 		{
-			bit = (message[i] >> j & 1);
-			if (bit == 0)
-				kill(pid, SIGUSR2);
-			else
+			if (message[i] >> j & 1)
 				kill(pid, SIGUSR1);
-			usleep(50);
+			else
+				kill(pid, SIGUSR2);
 			j--;
+			while (!g_response)
+				usleep(250);
+			g_response = 0;
 		}
 		i++;
 	}
@@ -39,7 +49,6 @@ void	send_message(int pid, char *message)
 int	main(int argc, char **argv)
 {
 	int		pid;
-	char	*message;
 
 	if (argc != 3)
 	{
@@ -47,9 +56,10 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 	pid = ft_atoi(argv[1]);
-	message = ft_strdup(argv[2]);
 
-	send_message(pid, message);
+	signal(SIGUSR1, &response_handler);
+
+	send_message(pid, argv[2]);
 
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,41 +6,41 @@
 /*   By: cayamash <cayamash@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 15:55:36 by cayamash          #+#    #+#             */
-/*   Updated: 2025/01/08 14:28:31 by cayamash         ###   ########.fr       */
+/*   Updated: 2025/01/08 16:48:36 by cayamash         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-t_message	g_message = {0, 0, 0};
+static t_message	g_message = {0, 0, 0};
 
-void	handler(int signal)
+void	signal_handler(int signal, siginfo_t *info, void *context)
 {
-	char	*msg;
-
+	//char	*msg;
+	(void)context;
 	g_message.c = g_message.c << 1;
 	if (signal == SIGUSR1)
-	{
 		g_message.c = g_message.c | 1;
-	}
 	g_message.i++;
-	ft_printf("c = %c\n", g_message.c);
-	ft_printf("i = %i\n", g_message.i);
 	if (g_message.i == 8)
 	{
 		if (g_message.c == '\0')
-			ft_printf("%s", g_message.msg);
-		if (g_message.msg)
-		{
-			msg = ft_strjoin(g_message.msg, &g_message.c);
-			free(g_message.msg);
-			g_message.msg = msg;
-		}
-		else
-			g_message.msg = ft_strdup(&g_message.c);
+            write(1, "\n", 1); // Print a newline to separate messages
+        else
+            write(1, &g_message.c, 1);
+			//ft_printf("%s", g_message.msg);
+		// if (g_message.msg)
+		// {
+		// 	msg = ft_strjoin(g_message.msg, &g_message.c);
+		// 	free(g_message.msg);
+		// 	g_message.msg = msg;
+		// }
+		// else
+		// 	g_message.msg = ft_strdup(&g_message.c);
 		g_message.c = 0;
 		g_message.i = 0;
 	}
+	kill(info->si_pid, SIGUSR1);
 }
 
 int	main(void)
@@ -50,10 +50,11 @@ int	main(void)
 	ft_printf("Welcome to Caroline Server *-*\n");
 	ft_printf("PID Server: %d\n", getpid());
 
-	signal_received.sa_handler = handler;
-	signal_received.sa_flags = 0;
-	sigaction(SIGUSR2, &signal_received, NULL);
+	signal_received.sa_sigaction = signal_handler;
+	signal_received.sa_flags = SA_SIGINFO;
+
 	sigaction(SIGUSR1, &signal_received, NULL);
+	sigaction(SIGUSR2, &signal_received, NULL);
 
 	while (1)
 		pause();
